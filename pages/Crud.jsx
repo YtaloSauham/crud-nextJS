50f272df002632cd80d8c638824589572f8a13b2
Ytalo Sauham Ribeiro Silva <ytalo.sauhan@hotmail.com>
2021-10-05 23:22:46 -0300

    comitando a primeira versao do projeto apenas com o crud add

---
+++ pages/Crud.jsx
@@ -0,0 +1,75 @@
+import { useEffect, useState } from "react"
+import  axios  from "axios";
+ 
+const baseUrl="http://localhost:3001/users"
+
+
+export default function Crud(){
+
+    const [name,setName]=useState('');
+    const [email,setEmail]=useState('');
+    const [usuarios,setUsuarios]=useState([]);
+
+    function renderTable(){
+        return (
+                
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>E-mail</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {renderRows()}
+                </tbody>
+            </table>
+        )
+    }
+    
+    function renderRows(){
+        return usuarios?.map((dados,i)=>{
+            return  (
+                <tr key={dados.id}> 
+                <td>{dados.id}</td>
+                    <td>{dados.name}</td>
+                    <td>{dados.email}</td>
+                    <td>
+                        <button onClick= {() => {"#"}}>Editar</button>
+                        <button onClick= {() => {"#"}}>Remover</button>
+                    </td>
+                
+                </tr>
+                    )
+         })
+    }
+
+    useEffect(()=>{
+    axios.get(baseUrl).then((res)=>setUsuarios(res.data))
+   
+    },[])
+   
+    async function enviarFormulario(){
+    await axios.post(baseUrl,{name,email})          
+    setName('')
+    setEmail('')
+     await axios.get(baseUrl).then((res)=>setUsuarios(res.data))
+    
+    
+}
+
+        return(
+            <div>
+            <input type="text" value={name} onChange={(e)=>setName(e.target.value)}/>
+            <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+            <button  onClick={enviarFormulario}>Enviar</button>
+            <ul>
+            {renderTable()}
+            </ul>
+            </div>
+        )
+
+    
+}
